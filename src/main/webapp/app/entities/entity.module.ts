95d75d251c247c1ee3289e752a6b75aba0f82652
Alejandro Ubalton <aleubalton@gmail.com>
2018-07-25 21:47:27 -0300

    Revert "Se agrega Persona y Mascota"
    This reverts commit 0452528d94154356a4a2828e34406ed4da311917.

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,13 +1,9 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-import { JhipsterPersonaMySuffixModule } from './persona-my-suffix/persona-my-suffix.module';
-import { JhipsterMascotaMySuffixModule } from './mascota-my-suffix/mascota-my-suffix.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
-        JhipsterPersonaMySuffixModule,
-        JhipsterMascotaMySuffixModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
